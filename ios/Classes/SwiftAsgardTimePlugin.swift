7caa4e440efbf5dd1b180520985db5d2cfeebf8f
Sunny Kinger <sunnykinger@gmail.com>
2020-10-09 18:16:39 +0530

    Updated dependencies

--- ios/Classes/SwiftAsgardTimePlugin.swift
+++ ios/Classes/SwiftAsgardTimePlugin.swift
@@ -9,6 +9,7 @@ public class SwiftAsgardTimePlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
