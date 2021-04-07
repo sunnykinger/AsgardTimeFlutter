3efee267e7f74755ae9679a51d9c0e3f7714c01e
eldarkk <eldarkk@gmail.com>
2021-04-07 12:21:29 +0600

    debug prints added

--- ios/Classes/SwiftAsgardTimePlugin.swift
+++ ios/Classes/SwiftAsgardTimePlugin.swift
@@ -30,13 +30,16 @@ public class SwiftAsgardTimePlugin: NSObject, FlutterPlugin {
     } 
     else if (call.method == "getOfflineTime") {
       if let interval = CheckinHelper.shared.getCurrentOfflineTime() {
+        print("getCurrentOfflineTime interval: \(interval)")
             let date = Date(timeIntervalSince1970: interval)
+            print("date: \(date)")
                 let formatter = DateFormatter();
                 formatter.timeZone = TimeZone.current;
                 formatter.locale = Locale(identifier: "en_US_POSIX")
                 formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ";
                 let str = formatter.string(from: date);
                 var dateString = str
+                print("dateString: \(dateString)")
                 if str.hasSuffix("Z") {
                     dateString = str.replacingOccurrences(of: "Z", with: "+00:00") 
                 }
