7dc7f2345fb4735500fd00906c953f839bfa8248
Sunny <sunnykinger@gmail.com>
2021-09-21 14:53:05 +0530

    version update

--- android/src/main/kotlin/com/asgard/asgard_time/AsgardTimePlugin.kt
+++ android/src/main/kotlin/com/asgard/asgard_time/AsgardTimePlugin.kt
@@ -1,46 +1,43 @@
 package com.asgard.asgard_time
 
+
 import androidx.annotation.NonNull
-
-
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.text.SimpleDateFormat
 import java.util.*
 
 /** AsgardTimePlugin */
-class AsgardTimePlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class AsgardTimePlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "asgard_time")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    }else if(call.method == "getTime"){
-      val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ")
-      result.success(formatter.format(Date().time))
-    }else if(call.method == "getTimezone"){
-      val timezone = TimeZone.getDefault().id;
-      result.success(timezone)
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "asgard_time")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else if (call.method == "getTime") {
+            val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZZZZZ")
+            result.success(formatter.format(Date().time))
+        } else if (call.method == "getTimezone") {
+            val timezone = TimeZone.getDefault().id;
+            result.success(timezone)
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
